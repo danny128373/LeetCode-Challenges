80b21456521372bf6a519f00e5f4804d2b5525f2
Daniel Meza <daniel.meza29@yahoo.com>
2020-10-23 13:32:06 -0500

    Finished plusOne and rotate challenge

---
+++ plus_one.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in reversed(range(len(digits))):
+            if digits[i] == 9:
+                digit[i] = 0
+            else:
+                digits[i] += 1
+                return digits
