589c6d58ab3aabd1f4f400380718d67a94b70efa
Daniel Meza <daniel.meza29@yahoo.com>
2020-11-10 21:06:54 -0600

    Finished strings mix challenge

--- codewars/javascript/pigIt.js
+++ codewars/javascript/pigIt.js
@@ -1,5 +1,10 @@
 function pigIt(str){
+    newString = ''
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === ' ') {
+            newString += str[i]
+        }
+    }
   }
 
-    
-    console.log(pigIt('This is my string'))
+console.log(pigIt('This is my string'))
